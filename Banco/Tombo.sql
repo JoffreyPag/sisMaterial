d95d92c61285128db0222a28b9454fbfcef630f7
Joffr <joffreypag@gmail.com>
2018-03-27 16:40:23 -0300

    mudanca nas tabelas

---
+++ Banco/Tombo.sql
@@ -0,0 +1,18 @@
+CREATE TABLE etec_tombo(
+	numero_tombo varchar (20) NOT NULL,
+	numero_serie varchar (20) NOT NULL,	
+	id_setor int,
+    id_material int,
+	PRIMARY KEY(numero_tombo),
+    FOREIGN KEY(id_material) REFERENCES etec_materiais(id_material),
+	FOREIGN KEY(id_setor) REFERENCES etec_setor(id_setor)
+);
+
+INSERT INTO etec_tombo VALUES
+("2015007494", "BRJ446BZMS", 1, 1),
+("2015007499", "BRJ446BZN4", 1, 1),
+("2015007500", "BRJ446BZMX", 1, 1),
+("2015007516", "BRJ446BZMT", 1, 1),
+("2015007525", "BRJ451GTH6", 1, 1),
+("2015007527", "BRJ446BZNP", 1, 1),
+("2015007541", "BRJ451GTGT", 1, 1),
--- Banco/materias.sql
+++ Banco/materias.sql
@@ -1,19 +1,15 @@
 CREATE TABLE etec_materiais(
-	id_tombo int NOT NULL AUTO_INCREMENT,
-	numero_tombo varchar (20) NOT NULL,
-	numero_serie varchar (20) NOT NULL,	
+	id_material int NOT NULL AUTO_INCREMENT,	
 	especificacao TEXT,
 	acessorio TEXT,
-	id_polo int,
-	PRIMARY KEY(id_tombo),
-	FOREIGN KEY(id_polo) REFERENCES etec_polo(idpolo)
+	PRIMARY KEY(id_material)
 );
 
-INSERT INTO etec_materiais (id_tombo, numero_tombo, numero_serie, especificacao, acessorio, id_polo) VALUES
-(1, '1111111', 'BRJ446BZN4', 'espec 1', 'Cabo de força, teclado, mouse, mouse pad, CD.', 1),
-(2, '2222222', 'BRJ426BZN2', 'espec 2', 'Cabo de força, teclado, mouse, mouse pad, CD.', 2),
-(3, '3333333', 'BRJ436BZN5', 'espec 3', 'Cabo de força, teclado, mouse, mouse pad, CD.', 3),
-(4, '4444444', 'BRJ436BZN6', 'espec 4', 'Cabo de força, teclado, mouse, mouse pad, CD.', 4),
-(5, '5555555', 'BRJ246BZN4', 'espec 5', 'Cabo de força, teclado, mouse, mouse pad, CD.', 5),
-(6, '6666666', 'BRJ426BZN1', 'espec 6', 'Cabo de força, teclado, mouse, mouse pad, CD.', 6),
-(7, '7777777', 'BRJ416BZN2', 'espec 7', 'Cabo de força, teclado, mouse, mouse pad, CD.', 7);
+INSERT INTO etec_materiais (id_material, especificacao, acessorio) VALUES
+(1, 'Computador HP Elite All in one 800 G1 AiO NT B BR2', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(2, 'Desktop HP Compaq PRO 6305, 3.2 GHZ, 500 G de HD, 8 G de memória RAM', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(3, 'Estabilizador APC, 1000W, Bivolt, com 8 Entradas', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(4, 'Estabilizador SOL 2000', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(5, 'Mesa para computador', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(6, 'Monitor de LED HP backlit, 20 poledagas, HPV 206HZ', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
+(7, 'Nobreak black, Ragtech, 600 VA', 'Cabo de força, teclado, mouse, mouse pad, CD.');
