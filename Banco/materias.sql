ba8354544bc6d8d90af3be772b0a65b15f6e056d
Joffr <joffreypag@gmail.com>
2018-04-03 15:29:38 -0300

    alteracoes

--- Banco/materias.sql
+++ Banco/materias.sql
@@ -7,9 +7,9 @@ CREATE TABLE etec_materiais(
 
 INSERT INTO etec_materiais (id_material, especificacao, acessorio) VALUES
 (1, 'Computador HP Elite All in one 800 G1 AiO NT B BR2', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(2, 'Desktop HP Compaq PRO 6305, 3.2 GHZ, 500 G de HD, 8 G de memória RAM', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(3, 'Estabilizador APC, 1000W, Bivolt, com 8 Entradas', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(4, 'Estabilizador SOL 2000', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(5, 'Mesa para computador', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(6, 'Monitor de LED HP backlit, 20 poledagas, HPV 206HZ', 'Cabo de força, teclado, mouse, mouse pad, CD.'),
-(7, 'Nobreak black, Ragtech, 600 VA', 'Cabo de força, teclado, mouse, mouse pad, CD.');
+(2, 'Desktop HP Compaq PRO 6305, 3.2 GHZ, 500 G de HD, 8 G de memória RAM', 'Teclado, mouse, mouse pad, CD’s de instalação, cabo de força.'),
+(3, 'Estabilizador APC, 1000W, Bivolt, com 8 Entradas', 'Cabo de força.'),
+(4, 'Estabilizador SOL 2000', 'Cabo de força.'),
+(5, 'Mesa para computador', '-'),
+(6, 'Monitor de LED HP backlit, 20 poledagas, HPV 206HZ', 'Cabo de força.'),
+(7, 'Nobreak black, Ragtech, 600 VA', 'Cabo de força.');
