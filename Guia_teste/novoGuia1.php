0e739ed3777e64549fd85f98465547a9c3d96d67
Joffr <joffreypag@gmail.com>
2018-04-27 16:28:57 -0300

    implementações

--- Guia_teste/novoGuia1.php
+++ Guia_teste/novoGuia1.php
@@ -1,9 +1,13 @@
 <?php
+include_once("../conexao.php");
 //TODO:
 //LISTAR ALL MATERIAIS QUE TEM CADASTRADOS E DO LADO UM CHECK BOX
 //NA SEGUDA ETAPA IRA BUSCAR DOS CHECKBOX SELECIOANDOS QUAIS TOMBOS DE CADA MATERIAL TEM EM ESTOQUE
 //NA SEGUNDA ETAPA TERA CHECKBOX PARA CADA TOMBO DE TODOS OS MATERIAS PARA LEVAR PRA TERCEIRA ETAPA
 //NA TERCEIRA LISTA TODOS OS TOMBOS SELECIONADOS
+
+$sql = "SELECT id_permanente, especificacao, acessorio FROM etec_materiais_permanentes";
+$result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +17,29 @@
     <title>Novo Guia - Selecionar material</title>
 </head>
 <body>
+    <form action="novoGuia2.php" method="post">
+        <table>
+            <tr>
+                <th></th>
+                <th>Especificação</th>
+                <th>Acessorios</th>
+            </tr>
     
+        <?php
+            //<input type="checkbox" name="nTombos[]" value="'.$row['numero_tombo'].'">
+            while($row = mysqli_fetch_array($result)){
+
+                echo '<tr>
+                        <td>
+                            <input type="checkbox" name="materiais[]" value="'.$row['id_permanente'].':'.$row['especificacao'].':'.$row['acessorio'].'">
+                        </td>
+                        <td>'.$row['especificacao'].'</td>
+                        <td>'.$row['acessorio'].'</td>
+                    </tr>';
+            }
+        ?>
+        </table>
+        <input type="submit" value="Confirmar">
+    </form>
 </body>
 </html>
--- Guia_teste/novoGuia2.php
+++ Guia_teste/novoGuia2.php
@@ -1,5 +1,6 @@
 <?php
-//<input type="checkbox" name="nTombos[]" value="'.$row['numero_tombo'].'">
+include_once("../conexao.php");
+$materials = $_POST['materiais'];
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +10,37 @@
     <title>Novo Guia - Selecionar tombo</title>
 </head>
 <body>
-    
+    <form action="novoGuia3.php" method="post">
+        <?php 
+            foreach ($materials as $mat) {
+                $dtl = explode(':', $mat);
+
+                echo '<table border=1>
+                        <tr>
+                            <td colspan=2>'.$dtl[1].'</td>
+                        </tr>';
+                $sql = "SELECT t.numero_tombo FROM etec_tombo t 
+                        INNER JOIN etec_departamento d ON t.id_departamento = d.id_departamento
+                        INNER JOIN etec_materiais_permanentes p ON p.id_permanente = ".$dtl[0]."
+                        WHERE d.isestoque = true AND t.id_material = p.id_permanente";
+                
+                $result = $conn->query($sql);
+                if($result->num_rows > 0){
+                    while($row = mysqli_fetch_array($result)){
+                        echo '<tr>
+                                <td><input type="checkbox" name="nTombos[]" value="'.$row['numero_tombo'].'"></td>
+                                <td>'.$row['numero_tombo'].'</td>
+                            </tr>';
+                    }
+                }else{
+                    echo '<tr>
+                            <td><p>Indisponivel</h4></p>
+                        </tr>';
+                }
+                echo '</table><br/>';
+            }
+        ?>
+        <input type="submit" value="Continuar">
+    </form>
 </body>
 </html>
